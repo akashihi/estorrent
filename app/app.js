4ed6cb071290c6cc7144de2933d4bb0fd14877fa
Denis Chapligin <me@chollya.org>
2016-01-06 12:16:33 +0200

    Angular UI bootstrap added

--- app/app.js
+++ app/app.js
@@ -2,11 +2,12 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+    'ui.bootstrap',
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
