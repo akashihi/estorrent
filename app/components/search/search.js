4a450fb605144b1bdf4c03f0ad9218efe67501a5
Denis Chapligin <me@chollya.org>
2016-01-06 17:32:32 +0200

    Implemented subcategory display

--- app/components/search/search.js
+++ app/components/search/search.js
@@ -35,8 +35,39 @@ estorrentSearch.controller('SearchCtl', function ($scope, euiHost, client, esFac
     });
 
     $scope.categoryClick = function(category) {
-        category.active = !category.active
-    }
+        /* Mark button */
+        category.active = !category.active;
+
+        /* Reload sub categories list */
+        $scope.selectedCategories = [];
+        $scope.filterCategories = []
+        $scope.categories.forEach(function(item) {
+            if (item.active) {
+                $scope.selectedCategories.push(item);
+                $scope.filterCategories.push(item.key)
+            }
+        });
+
+        if ($scope.filterCategories.length >0) {
+            client.search({
+                index: 'tpb',
+                type: 'torrent',
+                body: ejs.Request().agg(ejs.FilterAggregation('SubCategoryFilter').filter(ejs.TermsFilter('Category', $scope.filterCategories)).agg(ejs.TermsAggregation('categories').field('Subcategory').size(50)))
+            }).then(function (resp) {
+                $scope.SubCategories = resp.aggregations.SubCategoryFilter.categories.buckets;
+                $scope.errorSubCategories = null;
+            }).catch(function (err) {
+                $scope.SubCategories = null;
+                $scope.errorSubCategories = err;
+                // if the err is a NoConnections error, then the client was not able to
+                // connect to elasticsearch. In that case, create a more detailed error
+                // message
+                if (err instanceof esFactory.errors.NoConnections) {
+                    $scope.errorCategories = new Error('Unable to connect to elasticsearch.');
+                }
+            });
+        }
+    };
 
     $scope.nextPage = function () {
         log.console("here")
