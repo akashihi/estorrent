09b4e59fbc37ad4dd701c2d43e1b1c37b6cd055b
Denis Chapligin <me@chollya.org>
2016-01-06 13:21:50 +0200

    Added elastic ui.

--- app/app.js
+++ app/app.js
@@ -3,11 +3,13 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
     'ui.bootstrap',
+    'elasticui',
     'ngRoute',
     'myApp.view1',
     'myApp.view2',
     'myApp.version'
 ]).
+constant('euiHost', 'http://192.168.75.5:9200').
 config(['$routeProvider', function ($routeProvider) {
     $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
